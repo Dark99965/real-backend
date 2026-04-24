7d2c3b3c9e42ffd77f4552b58b1fec4ee68a57e7
Dark99965 <noah.retter@icloud.com>
2026-04-24 20:07:35 -0300

    h

--- WEEK 1/DAY ONE/README.md
+++
@@ -1,3 +0,0 @@
-# DAY ONE
-
-This is me exploring sqlite and mabey doing flask with it 
--- WEEK 1/DAY ONE/main.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("app.db")
-curser = conn.cursor()
-
-username = input("What's your name: \n")
-
-passwd = input("Whats ur passwd")
