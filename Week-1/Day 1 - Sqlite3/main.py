e5d2abc84b4d79b221495dd26e59c48a939f45b2
Dark99965 <noah.retter@icloud.com>
2026-04-24 20:33:11 -0300

    added db + finished mainpy

--- Week-1/Day 1 - Sqlite3/main.py
+++ Week-1/Day 1 - Sqlite3/main.py
@@ -8,3 +8,7 @@ username = input("What is Your name: \n")
 email = input("What is your email: \n")
 
 cursor.execute(f"INSERT INTO users VALUES (\"{username}\", \"{email}\")")
+
+cursor.execute("SELECT * FROM users")
+print(cursor.fetchall())
+conn.commit()
