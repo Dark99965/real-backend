1bd96ce889c345ecf372f92e8c5c9f3477499591
Dark99965 <noah.retter@icloud.com>
2026-04-28 17:26:11 -0300

    added sgin up page

--- Week-1/Day 2 - sqlite in web/main.py
+++ Week-1/Day 2 - sqlite in web/main.py
@@ -1,11 +1,23 @@
+# imports
+
 from flask import Flask
+from flask import render_template
+from flask import request
+import sqlite3
+# app
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return """
-    
-    <h1>Hello, world!</h1>
+# home route
 
-    """
+@app.route('/')
+def home():
+    email = request.args.get("email")
+    passwd = request.args.get("passwd")
+    if email and passwd:
+        conn = sqlite3.connect("app.db")
+        curser = conn.cursor()
+        curser.execute(f"insert into users values (?, ?)", (email, passwd))
+        conn.commit()
+        conn.close()
+    return render_template("index.html")
