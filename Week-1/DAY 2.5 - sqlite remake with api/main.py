bd63a734d5d0f3096d054914928183c3aa4385eb
Dark99965 <noah.retter@icloud.com>
2026-05-01 20:54:23 -0300

    hfe done use module

--- Week-1/DAY 2.5 - sqlite remake with api/main.py
+++ Week-1/DAY 2.5 - sqlite remake with api/main.py
@@ -8,6 +8,11 @@ from flask_sqlalchemy import SQLAlchemy
 # app
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 # home route
 
